d6b0be3e82a42e2d6ebdb927255125e2f6795557
Javier Moreno <email@javier-moreno.com>
2019-11-27 19:51:22 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-source /home/ec2-user/pid
-kill $PID
-java -jar /home/ec2-user/picalculatorwebservice-0.0.3-SNAPSHOT.jar > app.log &
-PID=$!
-echo export PID=$PID > /home/ec2-user/pid
+curl -X POST localhost:8080/shutdown
+java -jar /home/ec2-user/picalculatorwebservice-0.0.3-SNAPSHOT.jar > /home/ec2-user/app.log &
