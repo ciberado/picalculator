b95d7e2beeb3a496ae22354d363dab988920d9c3
Javier Moreno <email@javier-moreno.com>
2019-11-27 21:05:39 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "`date` shutting down previous server." >> /home/ec2-user/restart.log
-curl -X POST localhost:8080/shutdown
+curl -X POST localhost:8080/shutdown > /dev/null 2>&1
 sleep 10
 echo "`date` starting new server." >> /home/ec2-user/restart.log
 java -jar /home/ec2-user/picalculatorwebservice-0.0.3-SNAPSHOT.jar &
