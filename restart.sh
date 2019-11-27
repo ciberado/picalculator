d8e0fca73162d2f02e0c5236fe4f931baf338ce1
Javier Moreno <email@javier-moreno.com>
2019-11-27 19:08:55 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,6 +2,6 @@
 
 source pid
 kill $PID
-java -jar picalculatorwebservice-0.0.3-SNAPSHOT.jar > app.log &
+java -jar ~/picalculatorwebservice-0.0.3-SNAPSHOT.jar > app.log &
 PID=$!
 echo export PID=$PID > pid
